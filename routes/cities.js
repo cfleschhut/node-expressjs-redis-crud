d31a808728a9f447e25e74299159b4b08a495be4
Christian Fleschhut <weltrekordhalter@gmail.com>
2015-12-14 15:57:55 +0100

    add tests for creating new cities

--- routes/cities.js
+++ routes/cities.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+
+var cities = {
+  'Lotopia': 'description 1',
+  'Caspiana': 'description 2',
+  'Indigo': 'description 3'
+};
 
 router.route("/")
   .get(function(request, response) {
-    var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-    response.json(cities);
+    response.json(Object.keys(cities));
+  })
+
+  .post(parseUrlencoded, function(request, response) {
+    var newCity = request.body
+    cities[newCity.name] = newCity.description
+    response.status(201).json(newCity.name);
   });
 
 module.exports = router;
