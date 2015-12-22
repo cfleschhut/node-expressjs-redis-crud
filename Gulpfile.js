c2bc5b5430b504c8a7b69db11ef66f1517c0b3d1
Christian Fleschhut <weltrekordhalter@gmail.com>
2015-12-22 23:37:44 +0100

    add minify-css to gulp task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  minifyCss = require('gulp-minify-css');
 
 gulp.task('styles', function() {
   return gulp.src('./public/src/scss/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass())
+    .pipe(minifyCss())
     .pipe(gulp.dest('./public/dist/css'));
 });
 
