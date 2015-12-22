5667a1da50a0b4c2f0541b7ae705914b9644917b
Christian Fleschhut <weltrekordhalter@gmail.com>
2015-12-22 23:43:36 +0100

    add autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,15 @@
 
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
-  minifyCss = require('gulp-minify-css');
+  minifyCss = require('gulp-minify-css'),
+  autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   return gulp.src('./public/src/scss/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
     .pipe(minifyCss())
     .pipe(gulp.dest('./public/dist/css'));
 });
